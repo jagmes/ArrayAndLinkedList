979b800863c3a37d2d79d4bffefaa0fc86788e4c
jagmes <Gems8867@gmail.com>
2023-10-19 20:41:44 +0300

    ArrayList and LinkedList add: ДЗ к 13 лекции add: ДЗ к 15 лекции.

---
+++ src/hwlesson15/Main.java
@@ -0,0 +1,40 @@
+package hwlesson15;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<String, String>  namesOfEmployees  = new HashMap<>();
+
+        for (int i = 0; i <10000 ; i++) {
+            String uid =generateUid();
+
+            namesOfEmployees.put(uid,  new String());
+            System.out.println( namesOfEmployees.keySet());
+        }
+
+       namesOfEmployees.put(generateUid(),  new String());
+        System.out.println( namesOfEmployees.size());
+    }
+
+    public static String generateUid(){
+        int startLimit = 97; // буква 'a'
+        int endLimit = 122; // буква 'z'
+        int tLenght=1; //количество генерируемых символов.
+        Random random = new Random();
+   while (true) {
+       String generated = random.ints(startLimit, endLimit + 1).limit(tLenght).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+       if (generated.equals(26) )
+       tLenght++;
+        else {
+           return generated;
+       }
+
+      }
+    }
+
+}
